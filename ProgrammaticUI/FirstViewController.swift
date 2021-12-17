ffe5c6ce7196571bef6366210268873d74a93738
Tony-Kara <eniolaanthony@gmail.com>
2021-12-17 20:44:52 +0600

    Add shadow to fb button

--- ProgrammaticUI/FirstViewController.swift
+++ ProgrammaticUI/FirstViewController.swift
@@ -23,36 +23,53 @@ class FirstViewController: UIViewController {
         self.view.backgroundColor = UIColor.green
         setUpContent()
         setUpBackground()
-         setUpFbButton()
-         setUpEmailButton()
-         setUpGmailButton()
-         setUpHintLabel()
-         setUpTitleLabel()
+      //  styleFacebookButton()
+        setUpFbButton()
+        setUpEmailButton()
+        setUpGmailButton()
+        setUpHintLabel()
+        setUpTitleLabel()
         
         fbButton.addTarget(self, action: #selector(self.fbButtonPress), for: .touchUpInside)
         gmailButton.addTarget(self, action: #selector(self.gmailButtonPress), for: .touchUpInside)
-         emailButton.addTarget(self, action: #selector(self.emailButtonPress), for: .touchUpInside)
+        emailButton.addTarget(self, action: #selector(self.emailButtonPress), for: .touchUpInside)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.shadowColor = UIColor.black.cgColor
+        shadowLayer.shadowPath = UIBezierPath(roundedRect: fbButton.bounds, cornerRadius: 0).cgPath
+        shadowLayer.shadowOffset = CGSize(width: 5.0, height: 10.0)
+        shadowLayer.shadowOpacity = 0.7
+        shadowLayer.shadowRadius = 10
+        fbButton.layer.insertSublayer(shadowLayer, at: 0)
+     }
+    
     @objc func fbButtonPress(){
-     print("fb button pressed")
+        
+        print("fb button pressed")
         let newViewController = SecondViewController()
         newViewController.modalPresentationStyle = .fullScreen
         newViewController.title = "Second Screen"
         self.navigationController?.pushViewController(newViewController, animated: true)
         
-     }
-
-     @objc func gmailButtonPress(){
-     print("gmail button pressed")
-     }
-
-     @objc func emailButtonPress(){
-     print("email button pressed")
-     }
-
+    }
+    
+    @objc func gmailButtonPress(){
+        print("gmail button pressed")
+    }
+    
+    @objc func emailButtonPress(){
+        print("email button pressed")
+    }
+    
+    func styleFacebookButton() {
+        fbButton.layer.cornerRadius = 20.0
+        fbButton.layer.masksToBounds = true
+    }
+    
     func setUpContent(){
-     //code to be added
+        //code to be added
         self.view.addSubview(contentView)
         contentView.addSubview(background)
         contentView.addSubview(fbButton)
@@ -63,74 +80,74 @@ class FirstViewController: UIViewController {
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
         contentView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-         contentView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-         contentView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-         contentView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
-     }
+    }
     
     func setUpBackground(){
         background.image = UIImage(named: "Background")
         background.translatesAutoresizingMaskIntoConstraints = false
         background.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-         background.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
-         background.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-         background.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-     }
-
-     func setUpFbButton(){
-         fbButton.setBackgroundImage(UIImage(named: "FB"), for: .normal)
-         fbButton.setTitle("Sign in with Facebook", for: .normal)
-         fbButton.setTitleColor(.black, for: .normal)
-         fbButton.translatesAutoresizingMaskIntoConstraints = false
-         
-         fbButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
-          fbButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
-          fbButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-          fbButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-     }
-
-     func setUpEmailButton(){
-         emailButton.setBackgroundImage(UIImage(named: "Email"), for: .normal)
-         emailButton.setTitle("Sign in with Email", for: .normal)
-         emailButton.setTitleColor(.black, for: .normal)
-         emailButton.translatesAutoresizingMaskIntoConstraints = false
-         
-         emailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
-          emailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
-          emailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-          emailButton.topAnchor.constraint(equalTo: gmailButton.bottomAnchor,constant: 20).isActive = true
-     }
-
-     func setUpGmailButton(){
-         gmailButton.setBackgroundImage(UIImage(named: "Google"), for: .normal)
-         gmailButton.setTitle("Sign in with Google", for: .normal)
-         gmailButton.setTitleColor(.black, for: .normal)
-         gmailButton.translatesAutoresizingMaskIntoConstraints = false
-         
-         gmailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
-          gmailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
-          gmailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-          gmailButton.topAnchor.constraint(equalTo: fbButton.bottomAnchor,constant: 20).isActive = true
-     }
-
-     func setUpHintLabel(){
-         hintLabel.text = "Login or create an account"
-         hintLabel.translatesAutoresizingMaskIntoConstraints = false
-         
-         hintLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-          hintLabel.bottomAnchor.constraint(equalTo: fbButton.topAnchor, constant: -20).isActive = true
-     }
-
-     func setUpTitleLabel(){
-         titleLabel.text = "Social \nSquid"
-         titleLabel.numberOfLines = 0
-         titleLabel.font = UIFont(name: "AvenirNext-Regular", size: 44)
-         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-         
-         titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-          titleLabel.bottomAnchor.constraint(equalTo: hintLabel.topAnchor, constant: -50).isActive = true
-     }
+        background.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        background.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        background.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    func setUpFbButton(){
+        fbButton.setBackgroundImage(UIImage(named: "FB"), for: .normal)
+        fbButton.setTitle("Sign in with Facebook", for: .normal)
+        fbButton.setTitleColor(.black, for: .normal)
+        fbButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        fbButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        fbButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        fbButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        fbButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+    
+    func setUpEmailButton(){
+        emailButton.setBackgroundImage(UIImage(named: "Email"), for: .normal)
+        emailButton.setTitle("Sign in with Email", for: .normal)
+        emailButton.setTitleColor(.black, for: .normal)
+        emailButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        emailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        emailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        emailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        emailButton.topAnchor.constraint(equalTo: gmailButton.bottomAnchor,constant: 20).isActive = true
+    }
+    
+    func setUpGmailButton(){
+        gmailButton.setBackgroundImage(UIImage(named: "Google"), for: .normal)
+        gmailButton.setTitle("Sign in with Google", for: .normal)
+        gmailButton.setTitleColor(.black, for: .normal)
+        gmailButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        gmailButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 50).isActive = true
+        gmailButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -50).isActive = true
+        gmailButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        gmailButton.topAnchor.constraint(equalTo: fbButton.bottomAnchor,constant: 20).isActive = true
+    }
+    
+    func setUpHintLabel(){
+        hintLabel.text = "Login or create an account"
+        hintLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        hintLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        hintLabel.bottomAnchor.constraint(equalTo: fbButton.topAnchor, constant: -20).isActive = true
+    }
+    
+    func setUpTitleLabel(){
+        titleLabel.text = "Social \nSquid"
+        titleLabel.numberOfLines = 0
+        titleLabel.font = UIFont(name: "AvenirNext-Regular", size: 44)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: hintLabel.topAnchor, constant: -50).isActive = true
+    }
     
     
     
